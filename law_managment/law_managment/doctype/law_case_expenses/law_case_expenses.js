ec8a1c61ba0571eb2e2d223ac84c30b6ac10ae85
aldanaco <aldanacomputer1@gmail.com>
2025-01-22 17:20:12 +0000

    finishing

---
+++ law_managment/law_managment/doctype/law_case_expenses/law_case_expenses.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Asymptotes and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Law Case Expenses", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ law_managment/law_managment/doctype/law_case_expenses/test_law_case_expenses.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Asymptotes and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestLawCaseExpenses(FrappeTestCase):
+	pass
